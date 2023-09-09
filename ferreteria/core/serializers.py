cb6eb7314259cf3c54bd2c8e254c1b15f883cf5a
Salvador Ponce <salvaponce92@gmail.com>
2023-09-09 21:36:14 +0200

    fixing serializer

--- ferreteria/core/serializers.py
+++ ferreteria/core/serializers.py
@@ -4,28 +4,6 @@ from django.contrib.auth.models import User
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    """
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True, allow_blank=False, max_length=60)
-    description = serializers.CharField(style={"base_template": "textarea.html"})
-    image = serializers.ImageField(required=False, upload_to="product")
-    stock = serializers.BooleanField(default=False)
-    category = serializers.CharField(max_length=50, default="Otras")
-    price = serializers.FloatField()
-
-    def create(self, validated_data):
-        return Product.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.description = validated_data.get("description", instance.description)
-        instance.image = validated_data.get("image", instance.image)
-        instance.stock = validated_data.get("stock", instance.stock)
-        instance.category = validated_data.get("category", instance.category)
-        instance.price = validated_data.get("price", instance.price)
-        instance.save()
-        return instance"""
-
     class Meta:
         model = Product
         fields = "__all__"
@@ -51,4 +29,4 @@ class CartSerializer(serializers.ModelSerializer):
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ("id", "username", "email", "date_joined", "is_active", "is_staff")
+        fields = "__all__"
