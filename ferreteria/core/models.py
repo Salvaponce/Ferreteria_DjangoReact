d32f6f53f30d28271e9872fcf68502a50f7723d9
Salvador Ponce <salvaponce92@gmail.com>
2023-09-10 22:05:04 +0200

    changing little things in model and views

--- ferreteria/core/models.py
+++ ferreteria/core/models.py
@@ -36,20 +36,22 @@ class CartItem(models.Model):
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None):
         if not username:
-            raise ValueError("El nombre de usuario es obligatorio.")
+            raise ValueError("Users must have a username.")
         if not email:
-            raise ValueError("La dirección de correo electrónico es obligatoria.")
+            raise ValueError("Users must have an email.")
 
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-        )
-
+        user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None):
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError("Superusers must have a password.")
+        if email is None:
+            raise TypeError("Superusers must have an email.")
+        if username is None:
+            raise TypeError("Superusers must have an username.")
         user = self.create_user(
             username=username,
             email=email,
--- ferreteria/core/views.py
+++ ferreteria/core/views.py
@@ -19,19 +19,6 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
-"""class Product_List(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many = True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = ProductSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)"""
-
 
 def front(request):
     context = {}
@@ -42,35 +29,6 @@ def front(request):
 class Product_List(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-
-# The create() method of our serializer will now be passed an additional 'owner' field, along with the validated data from the request.
-
-
-"""class Product_Detalle(APIView):
-    def get_object(self, id):
-        try:
-            product = Product.objects.get(id)
-        except Product.DoesNotExist:
-            return Response(status = status.HTTP_404_NOT_FOUND)
-    
-    def get(self, id, request):
-        product = self.get_object(id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    
-    def put(self, id, request):
-        product = self.get_object(id)
-        serializer = ProductSerializer(product, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, id, request):
-        product = self.get_object(id)
-        product.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)"""
 
 
 class Product_Detalle(generics.RetrieveUpdateDestroyAPIView):
